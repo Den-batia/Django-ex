0f38ceaebbfb79a16721780dda5f402b98d62172
otherr <den.batia@gmail.com>
2020-04-21 18:19:06 +0300

    4

--- NewJ/app/views.py
+++ NewJ/app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpRequest
 from django.http import HttpResponse
+import time
 
 # Create your views here.
 def index(request):
+
     return render(request, 'app/index.html')
+
+def a(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return redirect(index)
+    return render(request, 'app/a.html')
